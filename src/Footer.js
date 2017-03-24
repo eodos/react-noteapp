b25b4f3ce5c93101ddb46f7fcd6f49bfafbc4344
David Paul Pena <dpaul@nyu.edu>
2017-03-24 12:49:19 -0400

    Cleanup

--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Footer.css';
 
-class Footer extends Component {
-	render() {
-		return (
-			<div className="Footer">
-				by David
-			</div>
-		)
-	}
-}
+const Footer = () =>
+	(
+		<div className="Footer">
+			by David
+		</div>
+	);
 
 export default Footer;
--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Header.css';
 
-class Header extends Component {
-	render() {
-		return (
-			<div className="Header">
-				<h1>Note App</h1>
-			</div>
-		)
-	}
-}
+const Header = () =>
+	(
+		<div className="Header">
+			<h1>Note App</h1>
+		</div>
+	);
 
 export default Header;
